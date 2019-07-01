efefbe64ee46c83bb4000edfce5fa05f019bab5a
Abishkar <abishkarchhetri@gmail.com>
2019-07-01 15:22:08 +0545

    Revert "Clean up folder organization"
    This reverts commit 5847f612d7329b0e93fd461253aeed75de3e8c3c.

--- BucketsAndTiles/Assets/SimpleDragAndDrop/Scripts/DummyControlUnit.cs
+++ BucketsAndTiles/Assets/SimpleDragAndDrop/Scripts/DummyControlUnit.cs
@@ -36,6 +36,16 @@ public class DummyControlUnit : MonoBehaviour
     }
 
     void Start() {
+        // GameObject g3 = (GameObject)(Instantiate (cell, transform.position, Quaternion.identity));
+        // var foundObjects = FindObjectsOfType<Cell>();
+        // var tiles = GameObject.Find("t1");
+        // var gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        // for (int i = 0; i < 2; i++) {
+        //     GameObject btn;
+        //     btn = Instantiate(tiles);
+        //     Debug.Log(gridLayoutGroup);
+        //     // btn.transform.SetParent(gridLayoutGroup.transform);
+        // }
     }
 
 
