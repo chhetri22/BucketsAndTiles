8371c1b972b3aed8a895f288e00f32e794857117
Meghana Vemulapalli <mpalli0012@gmail.com>
2019-07-02 01:23:24 -0400

    Clean DummyControlUnit and update mapping CSV

--- BucketsAndTiles/Assets/TilesAndBuckets/Scripts/DummyControlUnit.cs
+++ BucketsAndTiles/Assets/TilesAndBuckets/Scripts/DummyControlUnit.cs
@@ -5,20 +5,22 @@ using System.IO;
 using System.Collections.Generic;
 
 /// <summary>
-/// Example of control application for drag and drop events handle
+/// Handles the scoring based on drag and drop events;
+/// Generates array of correct answers and scoresbased on CSV
 /// </summary>
 public class DummyControlUnit : MonoBehaviour
 {
+    //this dict stores correct answers for each bucket
     public static Dictionary<string, List<string>> bucketToTilesMapping = new Dictionary<string, List<string>>();
 
-    //adding variable score Functionality
+    //adding variable score functionality
     public static Dictionary<string, string> bucketToScoreMapping = new Dictionary<string, string>();
+
 
     private Dictionary<int, string> indexToTile = new Dictionary<int, string>();
     public GameObject cell;
-    private static string path = "Assets/TilesAndBuckets/data/mapping.csv";
+
     private static string pathMeghana2 = "Assets/TilesAndBuckets/data/mappingMatrixFormat.csv";
-    //private static int score = 0;
 
     
 
@@ -51,7 +53,6 @@ public class DummyControlUnit : MonoBehaviour
                     if(values[j][k].Equals("1"))
                     {
                         bucketToTilesMapping[values[j][0]].Add(indexToTile[k]);
-                        Debug.Log("yeehaw");
                     }
                 }
             }
@@ -59,16 +60,6 @@ public class DummyControlUnit : MonoBehaviour
     }
 
     void Start() {
-        // GameObject g3 = (GameObject)(Instantiate (cell, transform.position, Quaternion.identity));
-        // var foundObjects = FindObjectsOfType<Cell>();
-        // var tiles = GameObject.Find("t1");
-        // var gridLayoutGroup = GetComponent<GridLayoutGroup>();
-        // for (int i = 0; i < 2; i++) {
-        //     GameObject btn;
-        //     btn = Instantiate(tiles);
-        //     Debug.Log(gridLayoutGroup);
-        //     // btn.transform.SetParent(gridLayoutGroup.transform);
-        // }
     }
 
 
@@ -82,47 +73,14 @@ public class DummyControlUnit : MonoBehaviour
         DummyControlUnit sourceSheet = desc.sourceCell.GetComponentInParent<DummyControlUnit>();
         // Get control unit of destination cell
         DummyControlUnit destinationSheet = desc.destinationCell.GetComponentInParent<DummyControlUnit>();
-        //switch (desc.triggerType)                                               // What type event is?
-        //{
-        //    case DragAndDropCell.TriggerType.DropRequest:                       // Request for item drag (note: do not destroy item on request)
-        //        Debug.Log("Request " + desc.item.name + " from " + sourceSheet.name + " to " + destinationSheet.name);
-        //        break;
-        //    case DragAndDropCell.TriggerType.DropEventEnd:                      // Drop event completed (successful or not)
-        //        if (desc.permission == true)                                    // If drop successful (was permitted before)
-        //        {
-        //            Debug.Log("Successful drop " + desc.item.name + " from " + sourceSheet.name + " to " + destinationSheet.name);
-        //        }
-        //        else                                                            // If drop unsuccessful (was denied before)
-        //        {
-        //            Debug.Log("Denied drop " + desc.item.name + " from " + sourceSheet.name + " to " + destinationSheet.name);
-        //        }
-        //        break;
-        //    case DragAndDropCell.TriggerType.ItemAdded:                         // New item is added from application
-        //        Debug.Log("Item " + desc.item.name + " added into " + destinationSheet.name);
-        //        break;
-        //    case DragAndDropCell.TriggerType.ItemWillBeDestroyed:               // Called before item be destructed (can not be canceled)
-        //        Debug.Log("Item " + desc.item.name + " will be destroyed from " + sourceSheet.name);
-        //        break;
-        //    default:
-        //        Debug.Log("Unknown drag and drop event");
-        //        break;
-        //}
+
 
         if (desc.triggerType == DragAndDropCell.TriggerType.DropEventEnd)
         {
-            //Debug.Log("Lemme just destroy things");
-            //desc.destinationCell.RemoveItem();
-            Debug.Log("Request " + desc.item.name + " from " + sourceSheet.name + " to " + destinationSheet.name);
-            Debug.Log("HEYY Package from: " + desc.sourceCell + "to " + desc.destinationCell);
 
-            //foreach (string key in tilesToBucketMapping.Keys)
-            //{
-            //    Debug.Log("Key: " + key.ToCharArray);
-            //}
             string destinationCellName = desc.destinationCell.ToString().Split(' ')[0];
             string sourceCellName = desc.sourceCell.ToString().Split(' ')[0];
 
-            Debug.Log(destinationCellName);
 
             if (bucketToTilesMapping.ContainsKey(destinationCellName))
             {
@@ -137,8 +95,6 @@ public class DummyControlUnit : MonoBehaviour
                     ScoreScript.scoreValue -= 1;
                 }
             }
-            Debug.Log("Your score is: " + ScoreScript.scoreValue);
-            Debug.Log("Your item is: " + desc.item.name);
         }
 
     }
